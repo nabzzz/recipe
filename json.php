7ecbe1160cad9351699b56a212af6aa419a911ae
nabeel <nab.smartsurfer@gmail.com>
2015-05-16 17:11:41 -0400

    First commit

---
+++ json.php
@@ -0,0 +1,22 @@
+{"title":"Recipe Puppy","version":0.1,"href":"http:\/\/www.recipepuppy.com\/","results":[{"title":"Garlic
+ Dijon Grilling Sauce \r\n\t\t\r\n\t\r\n\t\t\r\n\t\r\n\t\t\r\n\t\r\n\t\r\n\r\n","href":"http:\/\/www
+.kraftfoods.com\/kf\/recipes\/garlic-dijon-grilling-sauce-56449.aspx","ingredients":"garlic, dijon mustard"
+,"thumbnail":"http:\/\/img.recipepuppy.com\/631164.jpg"},{"title":"Bruschetta With Roasted Garlic and
+ Cherry Tomatoes","href":"http:\/\/www.recipezaar.com\/Bruschetta-With-Roasted-Garlic-and-Cherry-Tomatoes-244281"
+,"ingredients":"garlic, italian bread","thumbnail":"http:\/\/img.recipepuppy.com\/199304.jpg"},{"title"
+:"Flatbread With Za?atar","href":"http:\/\/www.recipezaar.com\/Flatbread-With-Zaatar-178010","ingredients"
+:"garlic, pita bread","thumbnail":"http:\/\/img.recipepuppy.com\/567386.jpg"},{"title":"Garlic Butter
+ for Steaks and Mash Potatoes","href":"http:\/\/www.recipezaar.com\/garlic-butter-for-steaks-and-mash-potatoes-358993"
+,"ingredients":"butter, garlic","thumbnail":"http:\/\/img.recipepuppy.com\/642047.jpg"},{"title":"Garlic
+ Vinegar","href":"http:\/\/www.recipezaar.com\/Garlic-Vinegar-251602","ingredients":"garlic, garlic,
+ vinegar","thumbnail":"http:\/\/img.recipepuppy.com\/647882.jpg"},{"title":"Garlic Croutons","href":"http
+:\/\/allrecipes.com\/Recipe\/Garlic-Croutons\/Detail.aspx","ingredients":"butter, garlic","thumbnail"
+:"http:\/\/img.recipepuppy.com\/19040.jpg"},{"title":"Kittencal's Do-Ahead Garlic","href":"http:\/\/www
+.recipezaar.com\/Kittencals-Do-Ahead-Garlic-73596","ingredients":"garlic, vegetable oil","thumbnail"
+:"http:\/\/img.recipepuppy.com\/40503.jpg"},{"title":"Roasted Garlic","href":"http:\/\/allrecipes.com
+\/Recipe\/Roasted-Garlic\/Detail.aspx","ingredients":"garlic, olive oil","thumbnail":"http:\/\/img.recipepuppy
+.com\/30536.jpg"},{"title":"White Bean Spread With Garlic & Rosemary","href":"http:\/\/allrecipes.com
+\/Recipe\/White-Bean-Spread-With-Garlic--Rosemary\/Detail.aspx","ingredients":"garlic, olive oil","thumbnail"
+:"http:\/\/img.recipepuppy.com\/2268.jpg"},{"title":"Romano Cheese Crisp","href":"http:\/\/allrecipes
+.com\/Recipe\/Romano-Cheese-Crisp\/Detail.aspx","ingredients":"garlic, romano cheese","thumbnail":"http
+:\/\/img.recipepuppy.com\/9493.jpg"}]}
---
+++ result.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Recipe</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/slider.css" rel="stylesheet" type="text/css" media="all"/>
+<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+<body>
+   <div class="banner">
+		<div class="wrap">
+			<div class="header-top">
+				<div class="logo">
+					<h1><a href="index.php">Puppy Recipe</a></h1>
+				</div>
+			</div>
+		</div>
+   </div>
+  <div class="main">
+		<div class="wrap">
+			<div class="content-top">
+				<div class="top-box">
+					<h2>Recipe Search</h2>
+					<hr>
+						<div class="result_recipe_container">
+							<div class="result_left_section"><img src="http://img.recipepuppy.com/357759.jpg"></div>
+							<div class="result_right_section">
+							<h3>Death by Chocolate Overdose Cake</h3>
+							<p>This cake provides the chocoholic with 4 different layers of chocolate goodness! I know the recipe looks difficult but it's well worth the effort</p>
+							</div>
+							</span>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="wrap">
+			<div class="footer-text">
+				<div class="copy">
+					<p> © 2015 All rights Reserved | Design by Nabeel Sheikh</a></p>
+				</div>
+			</div>	
+		</div>
+	</div>
+</body>
+</html>
+    	
+    	
+            
